c5843f40166f931603f49b240f9dcdb911148f39
tiagocomti <tiago.alexandre.oliveira@hotmail.com>
2021-03-12 17:42:06 -0300

    testando

--- advanced/frontend/config/main.php
+++ advanced/frontend/config/main.php
@@ -2,7 +2,7 @@
 $params = array_merge(
 //    require __DIR__ . '/../../common/config/params.php',
 //    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
+    require __DIR__ . '/params.php'
 //    require __DIR__ . '/params-local.php'
 );
 
@@ -37,11 +37,41 @@ return [
                 [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
+                    'logFile' => '@frontend/runtime/logs/web.log',
                 ],
             ],
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                'index/index' => 'index/index',
+            ],
+        ],
+        'assetManager' => [
+            'basePath'=>'@webroot/asset/', // Change to the actual path here
+            'baseUrl'=>'@web/asset/', // Change to the actual path here
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'js'=>[]
+                ],
+                'yii\bootstrap\BootstrapPluginAsset' => [
+                    'js'=>[]
+                ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => []
+                ],
+                'yii2mod\slider\IonSliderAsset' => [
+                    'css' => [
+                        'css/normalize.css',
+                        'css/ion.rangeSlider.css',
+                        'css/ion.rangeSlider.skinFlat.css'
+                    ]
+                ],
+            ]
         ],
         /*
         'urlManager' => [
